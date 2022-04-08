6cb14b98fcba3bb9ac58c02a3c79064c240e9674
Elena <biertli@bk.ru>
2022-04-08 19:48:41 +0300

    first commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,35 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+//Task 6
+        int a= 12;
+        int b = 27;
+        int c = 44;
+        int d = 15;
+        int e = 9;
+        int result = a* (b + (c-d*e));
+        System.out.println(result);
+        int resultPlus = result * -1;
+        System.out.println(resultPlus);
+
+        // Task 7
+        int f =5;
+        int j =7;
+f = f+j;
+j = f-j;
+        System.out.println("b= " + j);
+f= f-j;
+        System.out.println("a= " + f);
+
+        //Task 8
+int k =523;
+int n;
+n = (k / 10)%10;
+        System.out.println(n);
+
+
+
 
     }
 }
+
